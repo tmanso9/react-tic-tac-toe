96414cc9057562335bd0511308772c2b9037cbee
tmanso9 <tmanso92@gmail.com>
2023-12-09 12:47:39 +0000

    implemented time travel

--- src/App.js
+++ src/App.js
@@ -73,12 +73,31 @@ function Board({ xIsNext, squares, onPlay }) {
 export default function Game() {
 	const [xIsNext, setXIsNext] = useState(true)
 	const [history, setHistory] = useState([Array(9).fill(null)])
-	const currentSquares = history[history.length - 1]
+	const [currentMove, setCurrentMove] = useState(0)
+	const currentSquares = history[currentMove]
 
-	function handlePlay (nextSquares){
-		setHistory([...history, nextSquares])
+	function handlePlay(nextSquares) {
+		const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
+		setHistory(nextHistory)
+		setCurrentMove(nextHistory.length - 1)
 		setXIsNext(!xIsNext)
 	}
+
+	const jumpTo = (nextMove) => {
+		setCurrentMove(nextMove)
+		setXIsNext(nextMove % 2 === 0)
+	}
+
+	const moves = history.map((squares, move) => {
+		const description =
+			move > 0 ? `Go to move #${move}` : `Go to game start`
+
+		return (
+			<li key={move}>
+				<button onClick={() => jumpTo(move)}>{description}</button>
+			</li>
+		)
+	})
 
 	return (
 		<div className="game">
@@ -90,7 +109,7 @@ export default function Game() {
 				/>
 			</div>
 			<div className="game-info">
-				<ol>{/*TODO*/}</ol>
+				<ol>{moves}</ol>
 			</div>
 		</div>
 	)
